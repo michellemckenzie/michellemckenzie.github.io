ec3d842c636f364314918350287c7a2e79ed9f1f
michellemckenzie <31011981+michellemckenzie@users.noreply.github.com>
2022-01-11 00:19:20 -0800

    home is

---
+++ des157b/script.js
@@ -0,0 +1,58 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section')
+    const buttonCircle = document.getElementById('btnCircle');
+
+    const bookmarktriangle = document.getElementsByClassName('bookmarktri');
+
+ 
+    let mode = 'dark';
+
+
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            
+            banner.className = 'switch';
+            document.querySelector('img').src = "images/lightcats.png";
+            
+            button.className = 'switch';
+
+            buttonCircle.style.animation = '1s darktolight forwards'; 
+
+            for (const tri of bookmarktriangle){
+                tri.style.backgroundColor = 'white';
+            }
+
+            for (const section of sections) {
+                section.className = 'switch';
+                section.style.backgroundColor = 'white';
+            }
+            mode = 'light';
+            
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            document.querySelector('img').src = "images/darkcats.png";
+        
+            button.removeAttribute('class');
+
+            buttonCircle.style.animation = '1s lighttodark forwards'; 
+
+            for (const tri of bookmarktriangle){
+                tri.style.backgroundColor = '#b2a09b';
+            }
+
+            for (const section of sections) {
+                section.style.backgroundColor = '#b2a09b';
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
