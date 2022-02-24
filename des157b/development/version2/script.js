5fce827822253fb372b21bdd1b58c07f76672113
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-02-24 10:06:48 -0800

    btn works

--- des157b/development/version2/script.js
+++ des157b/development/version2/script.js
@@ -2,30 +2,22 @@
     'use strict';
     console.log('reading js');
 
-    Parse.initialize("hJiXQ6wc2TdRMkGA54o1NCiwXXLgWYH1jZD2Ej9G","O70nCNvlNQG4m0FZODyfbkOhwcdzq2Ydhm7WQKYM");
-    // Parse server
-    Parse.serverURL = 'https://parseapi.back4app.com/';
-
-
-
-
-
-
-
-
-
-
     let menuButton = document.querySelector('.fa-solid.fa-bars');
     let menuClose = document.querySelector('.fa-solid.fa-xmark');
     let mobileMenu = document.getElementById('menuOptions');
 
     menuButton.addEventListener('touchstart', function(){
-        mobileMenu.style.display = "block";
+        mobileMenu.style.opacity= "1";
         mobileMenu.style.animation = "menuAppear 2s forwards";
     });
 
     menuClose.addEventListener('touchstart', function(){
-        mobileMenu.style.display = "none";
+        mobileMenu.style.opacity = "0";
+        mobileMenu.style.animation = "";
     });
+
+    Parse.initialize("hJiXQ6wc2TdRMkGA54o1NCiwXXLgWYH1jZD2Ej9G","O70nCNvlNQG4m0FZODyfbkOhwcdzq2Ydhm7WQKYM");
+    // Parse server
+    Parse.serverURL = 'https://parseapi.back4app.com/';
 
 })();
