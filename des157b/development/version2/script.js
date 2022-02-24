c68aa25f30f8216cdf99515663e1240849a1d824
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-02-24 10:18:21 -0800

    css fixes

--- des157b/development/version2/script.js
+++ des157b/development/version2/script.js
@@ -5,20 +5,19 @@
     Parse.initialize("hJiXQ6wc2TdRMkGA54o1NCiwXXLgWYH1jZD2Ej9G","skZpewzy0S51fkiUHbs9qjXncb4xXY2KfWbw2VKd");
     // Parse server
     Parse.serverURL = 'https://parseapi.back4app.com/';
-    
+
 
     let menuButton = document.querySelector('.fa-solid.fa-bars');
     let menuClose = document.querySelector('.fa-solid.fa-xmark');
     let mobileMenu = document.getElementById('menuOptions');
 
     menuButton.addEventListener('touchstart', function(){
-        mobileMenu.style.opacity= "1";
+        mobileMenu.style.display= "flex";
         mobileMenu.style.animation = "menuAppear 2s forwards";
     });
 
     menuClose.addEventListener('touchstart', function(){
-        mobileMenu.style.opacity = "0";
-        mobileMenu.style.animation = "";
+        mobileMenu.style.display ="none";
     });
     
     
