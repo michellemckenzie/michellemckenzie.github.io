eed92c31c6bb0f6cea8986fc2fc92dc9e91228bf
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-02-24 09:26:01 -0800

    working on version 2

---
+++ des157b/development/version2/script.js
@@ -0,0 +1,31 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    Parse.initialize("hJiXQ6wc2TdRMkGA54o1NCiwXXLgWYH1jZD2Ej9G","O70nCNvlNQG4m0FZODyfbkOhwcdzq2Ydhm7WQKYM");
+    // Parse server
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+
+
+
+
+
+
+
+
+
+
+    let menuButton = document.querySelector('.fa-solid.fa-bars');
+    let menuClose = document.querySelector('.fa-solid.fa-xmark');
+    let mobileMenu = document.getElementById('menuOptions');
+
+    menuButton.addEventListener('touchstart', function(){
+        mobileMenu.style.display = "block";
+        mobileMenu.style.animation = "menuAppear 2s forwards";
+    });
+
+    menuClose.addEventListener('touchstart', function(){
+        mobileMenu.style.display = "none";
+    });
+
+})();
