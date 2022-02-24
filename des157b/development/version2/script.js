13eeb9b00b468e6ebca1c49ee1f4205143b5a0ac
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-02-24 11:42:58 -0800

    v2 today

--- des157b/development/version2/script.js
+++ des157b/development/version2/script.js
@@ -7,19 +7,36 @@
     Parse.serverURL = 'https://parseapi.back4app.com/';
 
 
+    let mainPage = document.getElementById('main');
+    let uploadPage = document.getElementById('uploadPage');
     let menuButton = document.querySelector('.fa-solid.fa-bars');
     let menuClose = document.querySelector('.fa-solid.fa-xmark');
     let mobileMenu = document.getElementById('menuOptions');
+    let addOption = document.getElementById('addOption');
+    let homeOption = document.getElementById('homeOption');
 
     menuButton.addEventListener('click', function(){
         mobileMenu.style.display= "flex";
-        mobileMenu.style.animation = "menuAppear 2s forwards";
+        mobileMenu.style.animation = "menuAppear 1.2s forwards";
+
+        addOption.addEventListener('click', function(){
+            mainPage.style.display = "none";
+            uploadPage.style.display = "flex";
+            closeMenu();
+        });
+
+        homeOption.addEventListener('click', function(){
+            mainPage.style.display = "block";
+            uploadPage.style.display = "none";
+            closeMenu();
+        });
     });
 
-    menuClose.addEventListener('click', function(){
-        mobileMenu.style.animation = "menuDisappear 2s forwards";
-    });
+    menuClose.addEventListener('click', closeMenu);
     
+    function closeMenu(){
+        mobileMenu.style.animation = "menuDisappear 1.2s forwards";
+    }
     
 
 })();
