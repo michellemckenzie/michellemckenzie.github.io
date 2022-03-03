e246534b4d93d880a61ad0897edc761c6d561b41
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-03-03 12:55:00 -0800

    scrolling photos

--- des157b/development/version3/script.js
+++ des157b/development/version3/script.js
@@ -7,6 +7,8 @@
     // Parse server
     Parse.serverURL = 'https://parseapi.back4app.com/';
 
+    let convertApi = ConvertApi.auth({secret: 'HDy3UHldQEvnxgbY'});
+
 
     let mainPage = document.getElementById('mainPage');
     let uploadPage = document.getElementById('uploadPage');
@@ -20,7 +22,6 @@
     let currentPage = mainPage;
 
     constructPhotoGallery();
-    alert('Hello! Thank you for testing my project. There are still issues with uploading HEIF/HEIC images, so the project can only take JPG/PNG at the moment. A workaround for mobile users currently is submitting a screenshot of your image. For user testing, please submit a photo, read the about page, and look at your submitted photo on the homepage. User testing can be observed with the inspection tool open and the screen resized to iPhone X, with the console opened to observe images being uploaded. Images still need to be processed with JIMP and will take a little time to load, so after submission please refresh for now.');
 
     // render photos from b4a onto the html page into the respective categories
 
@@ -66,7 +67,7 @@
         // this is a good place to collect data from the other fields
         if (fileUploadControl.files.length > 0) {
             let file = fileUploadControl.files[0];
-            const fileName = fileUploadControl.files[0].name;
+            let fileName = fileUploadControl.files[0].name;
             const type = fileUploadControl.files[0].type;
             const size = fileUploadControl.files[0].size;
             const name = nameUploadControl.value;
@@ -74,32 +75,39 @@
             const category = selectedCategory.value;
             console.log(name);
             console.log(file);
-            console.log(fileName);
+            console.log("here ", fileName);
             console.log(size);
             console.log(type)
             console.log(description);
             console.log(selectedCategory.value)
-               
+
+            if (type == "image/heic" || type == "image/heif"){
+              convertPhoto(file);
+             
+            }
+            console.log('it worked??');
             uploadPhoto(name, file, description, fileName, category);
         }
 
-        async function uploadPhoto(name, file, description, fileName, category){
-            const newPhoto = new Parse.Object('uploads');
-            newPhoto.set('file', new Parse.File(fileName, file));
-            newPhoto.set('name', name);
-            newPhoto.set('description', description);
-            newPhoto.set('category',category);
-            //This is a good place to save data from the other fields to the database
-            try {
-              const result = await newPhoto.save();
-              console.log(result.id);
-              getNewPhoto(result.id);
-              console.log('success!');
-            } catch (error) {
-              console.error('Error while uploading the photo: ', error);
-            }
-        }
+      
     });
+
+    async function uploadPhoto(name, file, description, fileName, category){
+      const newPhoto = new Parse.Object('uploads');
+      newPhoto.set('image', new Parse.File(fileName, file));
+      newPhoto.set('name', name);
+      newPhoto.set('description', description);
+      newPhoto.set('category',category);
+      //This is a good place to save data from the other fields to the database
+      try {
+        const result = await newPhoto.save();
+        console.log(result.id);
+        getNewPhoto(result.id);
+        console.log('success!');
+      } catch (error) {
+        console.error('Error while uploading the photo: ', error);
+      }
+  }
     
     async function getNewPhoto(photoId){
         const records = Parse.Object.extend('uploads');
@@ -116,7 +124,7 @@
           const photoDescription = results[0].get('description');
           let photoCategory = results[i].get('category');
           // This is a good place to get data from the database fields
-          showUploadedPhoto(photoURL, photoTitle, photoDescription, photoCategory);
+          showUploadedPhoto(photoURL, photoCategory);
           // This is a good place to run a function that clears out the form, which you will write below.
           clearForm();
         } catch (error) {
@@ -124,7 +132,7 @@
         } 
       }
       
-      function showUploadedPhoto(photoURL, photoTitle, photoDescription, photoCategory){
+      function showUploadedPhoto(photoURL,photoCategory){
         let html = `<img src="${photoURL}">`;
         document.querySelector(`#${photoCategory} .images`).innerHTML += html;
       }
@@ -142,11 +150,11 @@
         try{
           const results = await query.find();
           for (let i = 0; i < results.length; i++){
-            let photoURL = results[i].get('file').url();
+            let photoURL = results[i].get('image').url();
             let photoTitle = results[i].get('name');
             let photoDescription = results[i].get('description');
             let photoCategory = results[i].get('category');
-            showUploadedPhoto(photoURL, photoTitle, photoDescription, photoCategory);
+            showUploadedPhoto(photoURL, photoCategory);
           }
         } catch (error) {
             console.error('Error while getting photo', error);
@@ -154,6 +162,24 @@
 
       }
 
+      async function convertPhoto(file){
+        console.log("1");
+        let params = convertApi.createParams()
+        console.log("2");
+        params.add('file', file)
+        console.log("3");
+        let result = await convertApi.convert('heic', 'jpg', params)
+        console.log("4");
+
+        // Get result file URL
+        let url = result.files[0]
+        console.log("5");
+        console.log(url);
+        console.log(url.FileName);
+        console.log(url.Url);
+        console.log("6");
+       uploadPhoto("michelle", url.Url, "my desc", url.FileName, "dog");
+      }
   
 
       
