cf1228058f44576f1803c28f51a8dbe2d43ae33b
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-03-02 22:58:17 -0800

    user testing

--- des157b/development/version2/script.js
+++ des157b/development/version2/script.js
@@ -126,4 +126,23 @@
         document.querySelector('#upload').reset();
       }
 
+      function convertToJPG(){
+        const { promisify } = require('util');
+        const fs = require('fs');
+        const convert = require('heic-convert');
+
+        (async () => {
+          const inputBuffer = await promisify(fs.readFile)('/path/to/my/image.heic');
+          const outputBuffer = await convert({
+            buffer: inputBuffer, // the HEIC file buffer
+            format: 'JPEG',      // output format
+            quality: 1           // the jpeg compression quality, between 0 and 1
+          });
+
+          await promisify(fs.writeFile)('./result.jpg', outputBuffer);
+        })();
+      }
+
+      
+
 })();
