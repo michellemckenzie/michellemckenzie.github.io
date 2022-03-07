0578a6af2622e8b1ca9529f3822d3418ce168674
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-03-06 21:23:11 -0800

    version 3 wip

--- des157b/development/version3/script.js
+++ des157b/development/version3/script.js
@@ -7,7 +7,6 @@
     // Parse server
     Parse.serverURL = 'https://parseapi.back4app.com/';
 
-    let convertApi = ConvertApi.auth({secret: 'HDy3UHldQEvnxgbY'});
 
 
     let mainPage = document.getElementById('mainPage');
@@ -15,18 +14,36 @@
     let aboutPage = document.getElementById("aboutPage");
     let menuButton = document.querySelector('.fa-solid.fa-bars');
     let menuClose = document.querySelector('.fa-solid.fa-xmark');
-    let mobileMenu = document.querySelector('#menuOptions');
+    let menu = document.querySelector('#menu');
     let addOption = document.getElementById('addOption');
     let homeOption = document.getElementById('homeOption');
     let aboutOption = document.getElementById('aboutOption');
     let currentPage = mainPage;
+    let title = document.querySelector('#title');
+    let shoeLink = document.querySelector('#shoes h2');
+    let shoePage = document.getElementById('shoePage');
+
+    shoeLink.addEventListener('click', function(){
+      shoePage.style.display = 'block';
+      currentPage.style.display = 'none';
+      currentPage = shoePage;
+    });
+
+    title.addEventListener('click', function(){
+      if(currentPage != mainPage){
+        currentPage.style.display = 'none';
+        mainPage.style.display = 'flex';
+        currentPage = mainPage;
+      }
+    });
 
     constructPhotoGallery();
 
     // render photos from b4a onto the html page into the respective categories
 
     menuButton.addEventListener('click', function(){
-        mobileMenu.style.animation = "menuAppear 1s forwards";
+        menu.classList.add("show");
+        document.body.style.overflow = "hidden";
         
         addOption.addEventListener('click', function(){
             currentPage.style.display = "none";
@@ -53,7 +70,8 @@
     menuClose.addEventListener('click', closeMenu);
     
     function closeMenu(){
-        mobileMenu.style.animation = "menuDisappear 1s forwards";
+        menu.classList.remove("show");
+        document.body.style.overflow = "auto";
     }
 
     document.querySelector('#upload').addEventListener('submit', function(event){
@@ -73,37 +91,46 @@
             const name = nameUploadControl.value;
             const description = descriptionUploadControl.value;
             const category = selectedCategory.value;
-            console.log(name);
-            console.log(file);
-            console.log("here ", fileName);
-            console.log(size);
-            console.log(type)
-            console.log(description);
-            console.log(selectedCategory.value)
+            const convertedImg = false;
 
             if (type == "image/heic" || type == "image/heif"){
               convertPhoto(file);
-             
             }
-            console.log('it worked??');
-            uploadPhoto(name, file, description, fileName, category);
-        }
-
-      
+            else{
+              const fileAsDataURL = window.URL.createObjectURL(file);
+              handleImg(fileAsDataURL);
+              // uploadPhoto(name, file, description, fileName, category, convertedImg);
+            }
+        } 
     });
 
-    async function uploadPhoto(name, file, description, fileName, category){
-      const newPhoto = new Parse.Object('uploads');
-      newPhoto.set('image', new Parse.File(fileName, file));
-      newPhoto.set('name', name);
-      newPhoto.set('description', description);
-      newPhoto.set('category',category);
+    async function uploadPhoto(name, fileName, file, description, category, isResized){
+      const photo = new Parse.Object('uploads');
+      
+      
+
+      if (isResized){
+        photo.set('image', new Parse.File(fileName, { base64: resizedFile }));
+      }
+      else{
+        photo.set('image', new Parse.File(fileName, file));
+      }
+
+      photo.set('name', name);
+      photo.set('description', description);
+      photo.set('category',category);
+
+
       //This is a good place to save data from the other fields to the database
       try {
+        let loadingScreen = document.querySelector('#loadingScreen');
+        loadingScreen.style.display = 'flex';
         const result = await newPhoto.save();
         console.log(result.id);
         getNewPhoto(result.id);
+        loadingScreen.style.display='none';
         console.log('success!');
+        clearForm();
       } catch (error) {
         console.error('Error while uploading the photo: ', error);
       }
@@ -112,17 +139,15 @@
     async function getNewPhoto(photoId){
         const records = Parse.Object.extend('uploads');
         const query = new Parse.Query(records);
-        console.log('here');
         console.log(query);
-        console.log('end');
         // query.equalTo("objectId", photoId);
         try{
           const results = await query.find();
           console.log(results);
-          const photoURL = results[0].get('file').url();
+          // const photoURL = results[0].get('file').url();
           const photoTitle = results[0].get('name');
           const photoDescription = results[0].get('description');
-          let photoCategory = results[i].get('category');
+          // let photoCategory = results[i].get('category');
           // This is a good place to get data from the database fields
           showUploadedPhoto(photoURL, photoCategory);
           // This is a good place to run a function that clears out the form, which you will write below.
@@ -149,38 +174,91 @@
 
         try{
           const results = await query.find();
-          for (let i = 0; i < results.length; i++){
-            let photoURL = results[i].get('image').url();
-            let photoTitle = results[i].get('name');
-            let photoDescription = results[i].get('description');
+          for (let i = results.length-1; i >= 0; i--){
+
+            let photoURL;
+
+            if (results[i].get('image') === undefined){
+              photoURL = results[i].get('convertedImage');
+            }
+            else{
+              photoURL = results[i].get('image').url();
+            }
+            
             let photoCategory = results[i].get('category');
             showUploadedPhoto(photoURL, photoCategory);
           }
         } catch (error) {
             console.error('Error while getting photo', error);
         } 
-
       }
 
       async function convertPhoto(file){
-        console.log("1");
-        let params = convertApi.createParams()
-        console.log("2");
-        params.add('file', file)
-        console.log("3");
-        let result = await convertApi.convert('heic', 'jpg', params)
-        console.log("4");
+        let convertApi = ConvertApi.auth({secret: 'HDy3UHldQEvnxgbY'});
+        let params = convertApi.createParams();
+        params.add('file', file);
+        let result = await convertApi.convert('heic', 'jpg', params);
 
         // Get result file URL
-        let url = result.files[0]
-        console.log("5");
-        console.log(url);
-        console.log(url.FileName);
-        console.log(url.Url);
-        console.log("6");
-       uploadPhoto("michelle", url.Url, "my desc", url.FileName, "dog");
+        let url = result.files[0];
+
+        handleImg(url.Url);
+      //  uploadPhoto("michelle", url.Url, "my desc", url.FileName, "dog", convertedImg);
       }
-  
+
+      async function handleImg(imageUrl){
+        const dimensions = await getHeightAndWidthFromDataUrl(imageUrl);
+        resizeImg(imageUrl, dimensions);
+      }
+
+
+      async function resizeImg(imageUrl, dimensions){
+    
+        // make a square thumbnail for any image...
+        const image = await Jimp.read(imageUrl);
+        let squareThumbnail = resizePhoto(image, 250, 250);
+
+        let resizedImg;
+        const image2 = await Jimp.read(imageUrl);
+
+        let optimizeLandscapeWidth = 330;
+        let optimizePortraitHeight = 450;
+
+        if (dimensions.height > dimensions.width && dimensions.height > 450){
+          resizedImg = resizePhoto(image2, Jimp.AUTO, optimizePortraitHeight);
+        }
+        else if ((dimensions.height < dimensions.width || dimensions.height == dimensions.width) && dimensions.width > 330){
+          resizedImg = resizePhoto(image2, optimizeLandscapeWidth, Jimp.AUTO);
+        }
+        else{
+          resizeImg = imageUrl;
+        }
+
+        document.querySelector('#resized-img img').src = resizedImg;
+        document.querySelector('#square-img img').src = squareThumbnail;
+      }
+    
+      const getHeightAndWidthFromDataUrl = dataURL => new Promise(resolve => {
+        const img = new Image();
+        img.onload = () => {
+          resolve({
+            height: img.height,
+            width: img.width
+          });
+        }
+        img.src = dataURL;
+      });
+
+      function resizePhoto(image, width, height){
+        let newImg;
+        image.resize(width, height).getBase64('image/jpeg', (err, result) => {
+          if(err)
+              console.log(err);
+          else 
+            newImg = result;
+        });
+        return newImg;
+      }
 
       
 
