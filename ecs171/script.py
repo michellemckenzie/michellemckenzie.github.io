24ee13e60abc750553a15b97c1ad859db5846bbc
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-06-01 11:42:48 -0700

    adjust input width

--- ecs171/script.py
+++ ecs171/script.py
@@ -1,18 +1,19 @@
-from flask import Flask,render_template,request
+# importing Flask and other modules
+from flask import Flask, request, render_template
  
-app = Flask(__name__)
+# Flask constructor
+app = Flask(__name__)  
  
-@app.route('/form')
-def form():
-    return render_template('form.html')
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/', methods =["GET", "POST"])
+def input():
+    if request.method == "POST":
+       # getting input with name = sentence in HTML form
+       userinput = request.form.get("sentence")
+
+       print(userinput)
+    return render_template("index.html")
  
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
- 
- 
-app.run(host='localhost', port=5000)
+if __name__=='__main__':
+   app.run()
