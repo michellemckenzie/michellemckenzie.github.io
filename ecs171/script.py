29c725ffcd3db5b486e708130938941f546a5eeb
michellemckenzie <michellemckenzie@users.noreply.github.com>
2022-07-25 17:03:10 -0700

    mobile version for nft website

--- ecs171/script.py
+++ ecs171/script.py
@@ -1,19 +1,19 @@
-# importing Flask and other modules
-from flask import Flask, request, render_template
+# # importing Flask and other modules
+# from flask import Flask, request, render_template
  
-# Flask constructor
-app = Flask(__name__)  
+# # Flask constructor
+# app = Flask(__name__)  
  
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/', methods =["GET", "POST"])
-def input():
-    if request.method == "POST":
-       # getting input with name = sentence in HTML form
-       userinput = request.form.get("sentence")
+# # A decorator used to tell the application
+# # which URL is associated function
+# @app.route('/', methods =["GET", "POST"])
+# def input():
+#     if request.method == "POST":
+#        # getting input with name = sentence in HTML form
+#        userinput = request.form.get("sentence")
 
-       print(userinput)
-    return render_template("index.html")
+#        print(userinput)
+#     return render_template("index.html")
  
-if __name__=='__main__':
-   app.run()
+# if __name__=='__main__':
+#    app.run()
